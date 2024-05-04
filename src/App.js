94b16bdaac9adc5b87842a4b67d7c3574fd3c4a5
mohitBackendEngg <mohit.php@knobee.app>
2024-05-04 23:10:20 +0530

    changed css and html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <div style={{ width: "300px", height: "300px", background: "black", margin: "100px auto" }}></div>
+      <div style={{ fontSize: "20px", padding: "0 20px", borderRadius: "15px", display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "600", width: "300px", height: "300px", background: "Yellow", margin: "100px auto" }}>
+        Hello guys! , I am Jitesh Pandey
+      </div>
     </div>
   );
 }
