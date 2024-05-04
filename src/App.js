484464171724081a528d3e1e1a96262f637a7226
mohitBackendEngg <mohit.php@knobee.app>
2024-05-04 23:02:26 +0530

    changed the color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <div style={{ width: "300px", height: "300px", background: "yellow", margin: "100px auto" }}></div>
+      <div style={{ width: "300px", height: "300px", background: "black", margin: "100px auto" }}></div>
     </div>
   );
 }
